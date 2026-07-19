21b5335ee00482e4d754047f602a3e6393dae0e6
Fredrik93 <fredrikullman93@gmail.com>
2026-07-19 15:43:09 +0200

    add cors

--- src/main/java/com/birly/backend/controller/BillboardController.java
+++ src/main/java/com/birly/backend/controller/BillboardController.java
@@ -3,13 +3,10 @@ package com.birly.backend.controller;
 import com.birly.backend.Union;
 import com.birly.backend.dto.BillboardItemDTO;
 import com.birly.backend.service.BillboardService;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-
+@CrossOrigin(origins = "http://localhost:5173")
 @RestController
 @RequestMapping("/billboard")
 public class BillboardController {
