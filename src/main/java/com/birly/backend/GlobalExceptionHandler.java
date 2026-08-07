5fbe69db24d39cae8cc898497fabca0ccf84c5f6
Fredrik93 <fredrikullman93@gmail.com>
2026-08-07 05:32:30 +0200

    Add exception handling

--- src/main/java/com/birly/backend/GlobalExceptionHandler.java
+++ src/main/java/com/birly/backend/GlobalExceptionHandler.java
@@ -1,5 +1,7 @@
 package com.birly.backend;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -10,14 +12,22 @@ import java.util.Map;
 
 @RestControllerAdvice
 public class GlobalExceptionHandler {
-
+private static final Logger log = LoggerFactory.getLogger(GlobalExceptionHandler.class);
     @ExceptionHandler(MethodArgumentNotValidException.class)
     public ResponseEntity<Map<String, String>>
     handleValidationErrors(MethodArgumentNotValidException ex) {
         Map<String, String> errors = new HashMap<>();
         ex.getBindingResult().getFieldErrors().forEach(error -> errors.put(error.getField(), error.getDefaultMessage()));
-
+        log.debug(ex.getMessage());
         return ResponseEntity.badRequest().body(errors);
+    }
+
+    @ExceptionHandler(IllegalArgumentException.class)
+    public ResponseEntity<Map<String, String>> handleIllegalArgument(IllegalArgumentException ex) {
+        Map<String, String> error = new HashMap<>();
+        error.put("error", ex.getMessage());
+        log.debug(ex.getMessage());
+        return ResponseEntity.badRequest().body(error);
     }
 
     // Will by default catch all exceptions that are not explicitly handled in this class, like a safety net. Otherwise, lots of information is exposed to the client, like stack trace etc.
@@ -25,6 +35,7 @@ public class GlobalExceptionHandler {
     public ResponseEntity<Map<String, String>> handleGeneric(Exception ex) {
         Map<String, String> error = new HashMap<>();
         error.put("error", "Something went wrong.");
+        log.debug(ex.getMessage());
         return ResponseEntity.status(500).body(error);
     }
 }
